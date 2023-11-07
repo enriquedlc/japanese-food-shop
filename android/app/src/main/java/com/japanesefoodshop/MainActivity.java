ad17d7fb578b485f1f0d299c7a809f2e118f438f
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-11-07 23:54:00 +0000

    :sparkles::package: added navigation dependencies

--- android/app/src/main/java/com/japanesefoodshop/MainActivity.java
+++ android/app/src/main/java/com/japanesefoodshop/MainActivity.java
@@ -1,5 +1,6 @@
 package com.japanesefoodshop;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -8,7 +9,8 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate;
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
@@ -16,9 +18,16 @@ public class MainActivity extends ReactActivity {
     return "japanesefoodshop";
   }
 
+  @Override
+  protected String onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util
+   * class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and
+   * Concurrent React
    * (aka React 18) with two boolean flags.
    */
   @Override
