0d6ad726daab53883f6eefd2bd8b61eca2cc0ff9
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-11-13 18:53:53 +0000

    :art: tab bar assets

---
+++ assets/tab-bar/index.ts
@@ -0,0 +1,11 @@
+import BAG_FILLED from "./bag-filled.png";
+import BELL_FILLED from "./bell-filled.png";
+import HEART_FILLED from "./heart-filled.png";
+import HOME_FILLED from "./home-filled.png";
+
+export const TAB_BAR_ICONS = {
+  HOME_FILLED,
+  BELL_FILLED,
+  BAG_FILLED,
+  HEART_FILLED,
+} as const;
