02423cd45503d8fc22704ed0ca893d2d14bae06b
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-11-21 14:15:48 +0000

    :bug::fix: declare jpeg module and added food images

--- assets/index.ts
+++ assets/index.ts
@@ -1,5 +1,10 @@
 import SEARCH_ICON from "./search/search.png";
 
+// food
+import TERIYAKI_CHICKEN_BOWL from "./food/teriyaki-chicken-bowl.jpeg";
+import SUSHI_ROLL_COMBO from "./food/sushi-roll-combo.jpeg";
+import VEGETARIAN_RAMEN from "./food/vegetarian-ramen.jpeg";
+
 export const ICONS = {
   SEARCH_ICON,
 };
