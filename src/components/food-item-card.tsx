9e1203457db43a3b94dbaaa009247d98dfec4906
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-11-21 15:35:24 +0000

    :sparkles: card features like name and others moved to a separate component

--- src/components/food-item-card.tsx
+++ src/components/food-item-card.tsx
@@ -1,20 +1,11 @@
 import { LinearGradient } from "expo-linear-gradient";
-import {
-  Dimensions,
-  ImageProps,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { ImageProps, StyleSheet } from "react-native";
 
-import { ICONS } from "../../assets";
-import { COLORS } from "../theme/theme";
-import { BackGroundIcon } from "./background-icon";
+import { CardFeatures } from "./card-features";
 import { FoodItemCardImage } from "./food-item-card-image";
 import { Rating } from "./rating";
 
-const CARD_WIDTH = Dimensions.get("window").width * 0.3;
+import { COLORS } from "../theme/theme";
 
 interface FoodItemCardProps {
   id: string;
@@ -51,21 +42,11 @@ export function FoodItemCard(props: FoodItemCardProps) {
       <FoodItemCardImage image={image}>
         <Rating averageRating={averageRating} />
       </FoodItemCardImage>
-      <Text style={styles.cardTitle}>{name}</Text>
-      <Text style={styles.cardSubTitle}>{specialIngredient}</Text>
-      <View style={styles.cardFooter}>
-        <Text style={styles.cardPriceCurrency}>
-          <Text style={styles.cardPrice}>{price}</Text> €
-        </Text>
-        <TouchableOpacity>
-          <BackGroundIcon
-            backgroundColor={COLORS.primaryOrangeHex}
-            color={COLORS.primaryWhiteHex}
-            size={12}
-            name={ICONS.PLUS}
-          />
-        </TouchableOpacity>
-      </View>
+      <CardFeatures
+        name={name}
+        price={price}
+        specialIngredient={specialIngredient}
+      />
     </LinearGradient>
   );
 }
@@ -74,34 +55,5 @@ const styles = StyleSheet.create({
   linearCardGradient: {
     borderRadius: 25,
     padding: 15,
-  },
-
-  cardTitle: {
-    fontFamily: "poppins-medium",
-    color: COLORS.primaryWhiteHex,
-    fontSize: 14,
-    width: CARD_WIDTH,
-  },
-  cardSubTitle: {
-    fontFamily: "poppins-light",
-    color: COLORS.primaryWhiteHex,
-    fontSize: 12,
-  },
-  cardFooter: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginTop: 15,
-  },
-  cardPriceCurrency: {
-    fontFamily: "poppins-semibold",
-    color: COLORS.primaryOrangeHex,
-    fontSize: 16,
-  },
-  cardPrice: {
-    fontFamily: "poppins-semibold",
-    color: COLORS.primaryWhiteHex,
-    fontSize: 14,
-    lineHeight: 22,
   },
 });
